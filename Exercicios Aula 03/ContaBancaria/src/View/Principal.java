699ef6b789328b265fced2bf7ec66398a69371a7
kleber192 <79485180+kleber192@users.noreply.github.com>
2024-04-10 19:28:43 -0300

    Add files via upload

---
+++ Exercicios Aula 03/ContaBancaria/src/View/Principal.java
@@ -0,0 +1,119 @@
+package View;
+
+import Model.ContaModel;
+
+import java.util.Scanner;
+
+import Model.ContaEspecial;
+import Model.ContaPoupanca;
+
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+		
+    	ContaPoupanca p = new ContaPoupanca() ; 
+    	ContaEspecial e = new ContaEspecial() ;
+		char op ;
+		
+		Scanner s = new Scanner(System.in);
+		
+		
+		while(true) {
+			
+			System.out.println("Sitema de uso de Contas :  ");
+			
+			System.out.println(" ========================================");
+			
+			System.out.println("Digite opção para uso  :  ");
+			System.out.println(" Digite (A) para Cadastrar Poupanca ");
+			System.out.println(" Digite (B) para Cadastrar Especial ");
+			System.out.println(" Digite (E) para Sair ");
+			
+			op = s.nextLine().toUpperCase().charAt(0);
+			
+			if (op == 'A') {
+				
+				System.out.println("Sistema Cadastro de conta Poupanca !");	
+				cadastrarContaPoupanca(p, s);
+				
+				
+			}else if (op == 'C') {
+				
+				
+				System.out.println("Sistema Cadastro de conta Especial !");
+				cadastrarContaEspecial(e, s);
+				
+			}else if (op == 'E') {
+				
+				
+				
+				System.out.println("Sistema encerrando !");
+				System.exit(0);
+				
+			}
+			
+			 System.out.println("Sistema Encerrado !");
+		}
+			
+    
+	}
+	
+	
+	
+	
+	
+private static void cadastrarContaEspecial(ContaEspecial e, Scanner s) {
+		
+	
+	  System.out.println("Digite o nome :");
+	  e.setNome(s.nextLine());
+	  
+	  System.out.println("Digite o numero da conta :");
+	  e.setNumConta(s.nextInt());
+	  
+	  System.out.println("Digite o saldo inicial :");
+	  e.setSaldo(s.nextDouble());
+	
+	  e.setDepositar(1500);
+	  
+	  e.getSaque(300);
+	  
+	  
+	  e.getSaque(5000);
+	  
+	
+	
+		
+	}
+
+
+
+
+
+private static void cadastrarContaPoupanca(ContaPoupanca p, Scanner s) {
+		
+	  System.out.println("Digite o nome :");
+	  p.setNome(s.nextLine());
+	  
+	  System.out.println("Digite o numero da conta :");
+	  p.setNumConta(s.nextInt());
+	  
+	  System.out.println("Digite o saldo inicial :");
+	  p.setSaldo(s.nextDouble());
+	
+	
+	  p.setDepositar(3000);
+	  
+	  p.getSaque(150);
+	  
+	  p.calculadorNovoSaldo();
+	  
+	  
+	}
+
+
+
+
+}
